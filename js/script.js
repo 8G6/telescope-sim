5ce50a0bf24d5ccf386d48d344cc5b670f8a5dfa
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-12-16 01:48:28 +0530

    blur

--- js/script.js
+++ js/script.js
@@ -4,18 +4,26 @@ let planets = {
     "mars":16.64,
     "sat":20
 }
+
+
+
 let id   = (a)=>document.getElementById(a)
 let size = (a)=>id('obj').style.width = `${a*arcsec}%`
+let blur = (a)=>id('obj').style.filter = `blur(${0.115772*0.85/a})`
 let val  = (a)=>parseFloat(id(a).value)
 let sel  = (a)=>id(a).options[id(a).selectedIndex].value
+
 
 let opacity = (v) => id('space').style.opacity=`${v}%`
 
 function run(){
     id('obj').src = `./pics/planets/${sel('planets')}.png`
-
+    blur(val('dia') * parseFloat(sel("unit-dia")) )
+    let width = (val("FLM")*parseFloat(sel("unit-flm"))*parseFloat(sel("barlow")))/(val("FLE")*parseFloat(sel('unit-fle'))) 
+    // Fo / Fe * barlow
+     
     size(
-        (val("FLM")*parseFloat(sel("unit-flm"))*parseFloat(sel("barlow")))/(val("FLE")*parseFloat(sel('unit-fle'))) * planets[sel('planets')]
+         width * planets[sel('planets')]
     )
 
     id('obj').style.filter = `brightness(${100/parseFloat(sel("barlow"))**2}%)`
